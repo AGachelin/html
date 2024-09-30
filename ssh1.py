fd1160307e24bc9b91ae403456e8b9f73fce84c2
AGachelin <159674898+AGachelin@users.noreply.github.com>
2024-09-30 23:06:54 +0200

    fix(workflow): switch to secrets

--- ssh1.py
+++ ssh1.py
@@ -1,12 +1,12 @@
 from paramiko import client, AutoAddPolicy
-print("test successfull")
-# command = "cd html;git pull;cat README.md"
-command = "ls"
-host = "test"
-password = open("test.txt").read()
+import os
+command = "cd html;git pull;ls"
+host = os.environ['HOST']
+password = os.environ['PW']
+username = os.environ['ID']
 client = client.SSHClient()
 client.set_missing_host_key_policy(AutoAddPolicy())
-client.connect(host, username="agachelin", password=password)
+client.connect(host, username, password=password)
 _stdin, _stdout,_stderr = client.exec_command(command)
 print(_stdout.read().decode())
 client.close()
