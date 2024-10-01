6b686295c2ea83c003d0e1e2d11591ae7a69ef09
AGachelin <159674898+AGachelin@users.noreply.github.com>
2024-10-01 15:24:59 +0200

    intit : numerologie

---
+++ numerologie/numerologie.js
@@ -0,0 +1,41 @@
+function nombre(chaîne) {
+    let somme = 0
+    for (let i=0; i < chaîne.length; i++) {
+        somme += chaîne.charCodeAt(i)
+    }
+    return somme
+}
+
+// console.log(nombre("cou"))
+// console.log(nombre("coucou"))
+// for (let c of "cou") { 
+//     console.log(c + " : " + nombre(c))
+// }
+
+function somme(nombre) {
+    let somme = 0
+    chaîne = String(nombre)
+    for (let i=0; i < chaîne.length ; i++) {
+        somme += parseInt(chaîne.charAt(i))
+    }
+    return somme
+}
+
+// console.log(somme(132))
+// console.log(somme(4))
+// console.log("4".charCodeAt(0))
+// console.log("4".charAt(0))
+// console.log(typeof "4".charAt(0))
+// console.log(parseInt("4".charAt(0)))
+// console.log(typeof parseInt("4".charAt(0)))
+
+function chiffreAssocie(chaîne) {
+    valeur = nombre(chaîne)
+    while (valeur > 9) {
+        valeur = somme(valeur)
+    }
+    return valeur
+}
+
+// console.log(nombre("coucou"))
+// console.log(chiffreAssocie("coucou"))
