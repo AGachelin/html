72573ec606e767176e087e052334f07d9ebeddc8
mana-byte <manaikilaut@gmail.com>
2024-10-08 12:54:36 +0200

    fix(dice.js, html): rendering a scene and an object works

--- decathlon/script/dice.js
+++ decathlon/script/dice.js
@@ -1,13 +1,11 @@
-import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js";
-
 class Dice {
 	#value;
     cube;
 	constructor() {
 		this.#value = Math.floor(Math.random() * 6) + 1;
 		this.cube = new THREE.Mesh(
-			new Geometry(2, 2, 2),
-			new MeshBasicMaterial({ color: "purple" }),
+			new THREE.BoxGeometry(2, 2, 2),
+			new THREE.MeshBasicMaterial({ color: "purple" }),
 		);
 	}
 	get value() {
@@ -18,8 +16,6 @@ class Dice {
 	}
 }
 
-document.getElementById("throw").style.backgroundColor = "purple";
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
 	75,
@@ -28,20 +24,29 @@ const camera = new THREE.PerspectiveCamera(
 	1000,
 );
 const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-des = new Dice();
+const des = new Dice();
 
-document.querySelector("canvas").remove();
+// document.querySelector("canvas").remove();
 scene.background = new THREE.Color("#161718");
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-camera.setPosition(0, 0, 5);
+des.cube.position.set(0, 0, 0);
+des.cube.rotation.set(0.5, 0, 0);
+
+scene.add(des.cube);
 
 const light = new THREE.HemisphereLight("#FFFFFF", "#757575", 1.7);
 scene.add(light);
 
-des.cube.position.set(0, 0, 0);
-des.cube.rotation.set(0.5, 0, 0);
+camera.position.z = 5;
+
+const control = new THREE.OrbitControls(camera, renderer.domElement);
+const drag = () => {
+    control.update();
+}
+drag();
 
 renderer.render(scene, camera);
