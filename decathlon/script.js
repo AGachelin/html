bb0f4604ccd645a0a9ee9cb7f2fee834fc65ab69
AGachelin <159674898+AGachelin@users.noreply.github.com>
2024-10-08 10:06:38 +0200

    init(js): first commit

---
+++ decathlon/script.js
@@ -0,0 +1,19 @@
+class Dice{
+    constructor(number){
+        this.locked = false
+        this.number = number
+    }
+    remove_dice(){
+        this.locked = true
+    }
+
+}
+
+function throw_dices(){
+}
+
+function start_tentative(){
+}
+
+function end_tentative(){
+}
