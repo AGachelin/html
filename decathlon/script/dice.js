311794e870f43dac91141d3e2f5615caa19af0cd
AGachelin <159674898+AGachelin@users.noreply.github.com>
2024-10-16 00:32:27 +0200

    feat(js):3D models for dices

--- decathlon/script/dice.js
+++ decathlon/script/dice.js
@@ -1,12 +1,27 @@
 export class Dice {
     #value;
-	constructor() {
+	constructor(i, scene) {
+		let cube;
+		function loadModel(){
+			this.cube = cube;
+			this.cube.position.set(i-2, 0, 0);
+			this.cube.scale.setScalar( 0.5 );
+			scene.add(cube);
+		  }
+		this.manager = new THREE.LoadingManager( loadModel );
+		const loader = new THREE.OBJLoader( this.manager );
+		function onError() {}
+		loader.load( '../decathlon/models/dice.obj', function ( obj ) {
+			cube = obj;
+		}, onProgress, onError );		
 		this.#value = Math.floor(Math.random() * 6) + 1;
-		this.cube = new THREE.Mesh(
-			new THREE.BoxGeometry(1, 1, 1),
-			new THREE.MeshBasicMaterial({ color: "blue" }),
-		);
         this.locked = false;
+		function onProgress( xhr ) {
+			if ( xhr.lengthComputable ) {
+				const percentComplete = xhr.loaded / xhr.total * 100;
+				console.log( 'model ' + percentComplete.toFixed( 2 ) + '% downloaded' );
+			}
+		}
 	}
 	value() {
 		return this.#value;
--- decathlon/script/tentative.js
+++ decathlon/script/tentative.js
@@ -1,6 +1,6 @@
 import { Dice } from "./dice.js";
-
 class Tentative {
+	camera; scene; renderer; initialized;
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera(
 		75,
@@ -8,29 +8,39 @@ class Tentative {
 		0.1,
 		1000,
 	);
-	renderer = new THREE.WebGLRenderer();
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	constructor() {
+		this.show = this.show.bind(this);
+		this.initialized = false;
 		this.dices = [];
 		this.not_locked_dices = [];
 		for (let i = 0; i < 5; i++) {
-			this.dices.push(new Dice());
+			this.dices.push(new Dice(i, this.scene));
 			this.not_locked_dices.push(this.dices[i]);
+			this.show();
 		}
 		this.score = 0;
-		this.initialized = false;
 	}
 	init_scene() {
+		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
 		this.scene.background = new THREE.Color("#000000");
 		document.getElementById("dicethrow").appendChild(this.renderer.domElement);
-		this.dices.forEach((x, i) => {
-			x.cube.position.set(i * 3, 0, 0);
-			this.scene.add(x.cube);
-		});
-		const light = new THREE.HemisphereLight("#FFFFFF", "#757575", 1.7);
+		// this.dices.forEach((x, i) => {
+		// 	x.cube.position.y = - 0.95;
+		// 	x.cube.scale.setScalar( 0.01 );
+		// 	scene.add(x.cube);
+		// 	// x.cube.position.set(i * 3, 0, 0);
+		// 	// this.scene.add(x.cube);
+		// });
+		const light = new THREE.HemisphereLight("#FFFFFF", "#757575", 0.5);
 		this.scene.add(light);
 		this.camera.position.z = 6;
 		this.camera.position.x = 6;
+		const controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
+		controls.minDistance = 2;
+		controls.maxDistance = 5;
+		controls.addEventListener('change',this.show);
 	}
 	show() {
 		if (!this.initialized) {
