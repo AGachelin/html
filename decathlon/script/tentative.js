b585191c19babaa21e85bc3168d992b81f771811
mana-byte <manaikilaut@gmail.com>
2024-10-23 12:42:21 +0200

    Revert "added(tentative.js): selector working correctly, issue with first dice"
    This reverts commit c0eed91b5def680b5f1acb676435ae9e0f147a7a.

--- decathlon/script/tentative.js
+++ decathlon/script/tentative.js
@@ -1,84 +1,76 @@
 import { Dice } from "./dice.js";
 
-export class Tentative {
-    canvas;
-    renderer;
+class Tentative {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000,
+    );
+    renderer = new THREE.WebGLRenderer();
+
     pointer = new THREE.Vector2();
     raycaster = new THREE.Raycaster();
-    selectedDice;
 
     constructor() {
         this.show = this.show.bind(this);
-        const canvas = document.getElementById("canvas");
-        this.renderer = new THREE.WebGLRenderer({
-            canvas: canvas,
-            antialias: true,
-        });
+        this.initialized = false;
         this.dices = [];
         this.not_locked_dices = [];
         this.locked_dices = [];
         for (let i = 0; i < 5; i++) {
-            this.dices.push(new Dice(i));
+            this.dices.push(new Dice());
             this.not_locked_dices.push(this.dices[i]);
+            this.show();
         }
         this.score = 0;
     }
-
-    orbitControls(cam, scene) {
-        const controls = new THREE.OrbitControls(cam, scene.element);
+    orbitControls() {
+        const controls = new THREE.OrbitControls(
+            this.camera,
+            this.renderer.domElement,
+        );
+        controls.minDistance = 2;
+        controls.maxDistance = 5;
         controls.addEventListener("change", this.show);
-        controls.target.set(0, 0, 0);
     }
-
     async init_scene() {
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-        const Promise1 = Promise.all(
-            this.not_locked_dices.map((dice) => dice.loadModel()),
+        this.scene.background = new THREE.Color("#000000");
+        document.getElementById("dicethrow").appendChild(this.renderer.domElement);
+        const light = new THREE.HemisphereLight("#FFFFFF", "#757575", 0.5);
+        this.scene.add(light);
+        this.camera.position.z = 6;
+        this.camera.position.x = 6;
+
+        this.cube = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
         );
-        const Promise2 = Promise1.then((val) => {
-            for (let i = 0; i < this.not_locked_dices.length; i++) {
-                const sceneElement = document.createElement("div");
-                sceneElement.className = "list-item";
-                this.dices[i].scene.element = sceneElement;
-                document.getElementById("dicethrow").appendChild(sceneElement);
-                this.dices[i].scene.background = new THREE.Color("#000000");
-                this.dices[i].scene.add(
-                    new THREE.HemisphereLight("#FFFFFF", "#757575", 0.25),
-                );
-                const light = new THREE.DirectionalLight(0xffffff, 0.5);
-                light.position.set(-1, 2, 4);
-                this.dices[i].scene.add(light);
-                this.dices[i].scene.add(this.dices[i].cube);
-                this.dices[i].camera.position.set(i - 2, 1, 2);
-                this.dices[i].camera.updateProjectionMatrix();
-                this.orbitControls(this.dices[i].camera, this.dices[i].scene);
-            }
-        });
-        return Promise2;
+
+        await Promise.all(this.not_locked_dices.map((dice) => dice.loadModel()));
+
+        for (let i = 0; i < this.not_locked_dices.length; i++) {
+            this.dices[i].cube.position.set(i - 2, 0, 0);
+            this.dices[i].cube.rotation.set(0, 0, 0);
+            this.dices[i].cube.traverse((mesh) => {
+                mesh.material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+            });
+            this.scene.add(this.dices[i].cube);
+        }
+        this.cube.position.set(0, 2, 0);
+        this.scene.add(this.cube);
+        this.orbitControls();
     }
 
     show() {
-        this.renderer.setScissorTest(false);
-        this.renderer.setClearColor(0x0, 0);
-        this.renderer.clear(true, true);
-        this.renderer.setScissorTest(true);
-        for (let i = 0; i < this.not_locked_dices.length; i++) {
-            const element = this.dices[i].scene.element;
-            const { left, right, top, bottom, width, height } =
-                element.getBoundingClientRect();
-            this.dices[i].camera.aspect = width / height;
-            const positiveYUpBottom = this.renderer.domElement.clientHeight - bottom;
-            this.renderer.setScissor(left, positiveYUpBottom, width, height);
-            this.renderer.setViewport(left, positiveYUpBottom, width, height);
-            this.dices[i].camera.lookAt(
-                this.dices[i].scene.position.x,
-                this.dices[i].scene.position.y,
-                this.dices[i].scene.position.z,
-            );
-            this.dices[i].camera.updateProjectionMatrix();
-            this.renderer.render(this.dices[i].scene, this.dices[i].camera);
+        if (!this.initialized) {
+            this.init_scene();
+            this.initialized = true;
         }
+        this.renderer.render(this.scene, this.camera);
     }
     start_turn() {
         scores = [0, 0, 0, 0, 0, 0];
@@ -95,42 +87,22 @@ export class Tentative {
     }
     selector() {
         const onMouse = (event) => {
-            const wid = document
-                .getElementsByClassName("list-item")
-                .item(0)
-                .getBoundingClientRect().width;
-            this.pointer.x = ((event.clientX % wid) / wid) * 2 - 1;
-            this.pointer.y =
-                -(
-                    event.clientY /
-                    document
-                        .getElementsByClassName("list-item")
-                        .item(0)
-                        .getBoundingClientRect().height
-                ) *
-                2 +
-                1;
-            console.log(this.pointer);
-            const i = Math.floor(event.clientX / wid);
-            this.raycaster.setFromCamera(this.pointer, this.dices[i].camera);
+            this.pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+            this.pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+            this.raycaster.setFromCamera(this.pointer, this.camera);
             const intersects = this.raycaster.intersectObjects(
-                this.dices[i].scene.children,
+                this.scene.children,
                 true,
             );
-            if (intersects.length !== 0) {
-                console.log(intersects[0].object);
-                this.selectedDice = intersects[0].object;
-                for (let i = 0; i < this.not_locked_dices.length; i++) {
-                    this.not_locked_dices[i].cube.traverse((mesh) => {
-                        mesh.material.color.set("lime");
-                    });
-                }
-                this.selectedDice.material.color.set("red");
+            console.log(intersects);
+            for (let i = 0; i < intersects.length; i++) {
+                intersects[i].object.traverse((mesh) => {
+                    mesh.material.color.set("red");
+                });
+                console.log(intersects[i].object);
             }
             this.show();
         };
-        for (item in Array.from(document.getElementsByClassName("list-item"))) {
-            item.addEventListener("click", onMouse);
-        }
+        window.addEventListener("click", onMouse);
     }
 }
