c3f9354265f3186b101916cbc75f498d8fac24ce
mana-byte <manaikilaut@gmail.com>
2024-11-03 18:22:51 +0100

    fixed(main.js): bug where game html page would start even if no players

--- decathlon/script/main.js
+++ decathlon/script/main.js
@@ -6,7 +6,7 @@ const players = [];
 const addPlayer = () => {
     players.push(new Player(`Player ${players.length + 1}`));
     document.getElementById("player_list").innerHTML += `<li>${players[players.length - 1].name}</li>`;
-    console.log("player addded");
+    console.log("player added");
 };
 
 const playGame = async () => {
@@ -24,6 +24,9 @@ document.querySelector("#done-button").addEventListener("click", (event) => {
     document.getElementById("throw").style.backgroundColor = "white";
 });
 document.querySelector("#play-button").addEventListener("click", (event) => {
+    if(players.length === 0) {
+        return alert("Please add a player before playing the game");
+    }
     document.getElementById("div1").style.display = "none";
     document.getElementById("player_menu").style.display = "none";
     document.getElementById("div2").style.display = "block";
